2c5e858948d100f2a61196c60a262219f74f42d5
nljb <a@nljb.net>
2019-11-06 11:56:20 +0800

    update example.py

--- example/example.py
+++ example/example.py
@@ -19,6 +19,69 @@ def stop(client):
     data['params'] = params
     js = json.dumps(data)
     client.send((js + '\n#End\n').encode('utf-8'))
+
+def playText(client, zIndex, content, top, left, width, height, screenMode, screenRotate, color, bgcolor, font_size, align, style):
+    data = {}
+    params = {}
+    params['zIndex'] = zIndex
+    params['content'] = content
+    params['top'] = top
+    params['left'] = left
+    params['width'] = width
+    params['height'] = height
+    params['screen_mode'] = screenMode
+    params['screen_rotate'] = screenRotate
+    params['color'] = color
+    params['bgcolor'] = bgcolor
+    params['font_size'] = font_size
+    params['align'] = align
+    params['style'] = style
+    data['id'] = ("PLAY_TEXT_%d") % (int(round(time.time() * 1000)))
+    data['type'] = "play"
+    data['libName'] = 'text'
+    data['params'] = params
+    js = json.dumps(data)
+    client.send((js + '\n#End\n').encode('utf-8'))
+
+def playScroll(client, zIndex, content, top, left, width, height, screenMode, screenRotate, color, font_size, style, speed):
+    data = {}
+    params = {}
+    params['zIndex'] = zIndex
+    params['content'] = content
+    params['top'] = top
+    params['left'] = left
+    params['width'] = width
+    params['height'] = height
+    params['screen_mode'] = screenMode
+    params['screen_rotate'] = screenRotate
+    params['color'] = color
+    params['font_size'] = font_size
+    params['style'] = style
+    params['speed'] = speed
+    data['id'] = ("PLAY_SCROLL_%d") % (int(round(time.time() * 1000)))
+    data['type'] = "play"
+    data['libName'] = 'scroll'
+    data['params'] = params
+    js = json.dumps(data)
+    client.send((js + '\n#End\n').encode('utf-8'))
+
+def playScrollBackground(client, zIndex, top, left, width, height, screenMode, screenRotate, bgcolor):
+    data = {}
+    params = {}
+    params['zIndex'] = zIndex
+    params['top'] = top
+    params['left'] = left
+    params['width'] = width
+    params['height'] = height
+    params['screen_mode'] = screenMode
+    params['screen_rotate'] = screenRotate
+    params['bgcolor'] = bgcolor
+    data['id'] = ("PLAY_BACKGROUND_%d") % (int(round(time.time() * 1000)))
+    data['type'] = "play"
+    data['libName'] = 'background'
+    data['params'] = params
+    js = json.dumps(data)
+    client.send((js + '\n#End\n').encode('utf-8'))
     
 def play(client, libName, zIndex, path, top, left, width, height, screenMode, screenRotate):
     data = {}
@@ -52,6 +115,27 @@ if __name__ == '__main__':
 
     # 停止全部
     stop(client)
+
+    # 显示文本
+    playText(client,
+             3,
+             '专为树莓派(Raspberry Pi)设计的多媒体播放器',
+             0, 0, 1920, 50,
+             'landscape', 0,
+             'rgba(255, 0, 0, 100%)', 'rgba(255, 255, 255, 20%)', 30, 'center', 'bold')
+
+    # 显示滚动字幕
+    playScroll(client,
+               4,
+               '专为树莓派(Raspberry Pi)设计的多媒体播放器，支持(视频、音频、流媒体、图片、摄像头、动画、文本、滚动字幕、二维码)',
+               100, 0, -1, 50,
+               'landscape', 0, 'rgba(0, 0, 255, 100%)', 18, 'bold', 2)
+
+    # 显示滚动字幕透明背景
+    playScrollBackground(client,
+                         5,
+                         100, 0, 1920, 50,
+                         'landscape', 0, 'rgba(255, 255, 255, 20%)')
 
     # 循环播放
     while True:
