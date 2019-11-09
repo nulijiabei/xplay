3b806a294bba8e7562d807ff204357a22ea4c2cc
nljb <nljb@qq.com>
2019-11-09 02:12:24 +0000

    增加 RPI4 支持 Raspbian Buster with desktop

--- buster-lite/rpi4_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Successful installation -> /usr/bin/xplay"
--- buster/rpi4_install.sh
+++ buster/rpi4_install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Successful installation -> /usr/bin/xplay"
+dir=$(/usr/bin/dirname $0)
+
+cp -aRrf ${dir}/bin/xplay /usr/bin/
+cp -aRrf ${dir}/libs/rpi4/* /usr/local/lib/
+
+mkdir /etc/xplay
+cp -aRrf ${dir}/etc/log4qt.properties /etc/xplay/
+cp -aRrf ${dir}/etc/simsun.ttc /etc/xplay/
+cp -aRrf ${dir}/etc/00-xplay.conf /etc/ld.so.conf.d/
+ldconfig
+
+systemctl disable display-manager
+systemctl stop display-manager
+
+echo "Successful installation -> reboot -> /usr/bin/xplay"
