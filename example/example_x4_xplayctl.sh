acd6f78aa23bdab5f553c3f8c477acc366841b1d
nljb <nljb@qq.com>
2019-12-10 02:08:34 +0000

    update example example_x4_xplayctl.sh

--- example/example_x4_xplayctl.sh
+++ example/example_x4_xplayctl.sh
@@ -10,31 +10,34 @@ RECT_2="960,0,960,540"   # 位置 2 显示区域与位置
 RECT_3="0,540,960,540"   # 位置 3 显示区域与位置
 RECT_4="960,540,960,540" # 位置 4 显示区域与位置
 
+index=0
 while(true);do
-    index=0
     for v in `ls ${CONTENT}`;do
-        # 图片与视频原理相同 ...
         if [[ $v == *.jpg ]] || [[ $v == *.png ]];then
             if [ ${index} -eq 0 ];then
 	        echo Play PIC ${ZINDEX_1} ${RECT_1} "${CONTENT}/${v}"
 	        /usr/bin/xplayctl -play -libName pic -zIndex ${ZINDEX_1} -rect ${RECT_1} -path "${CONTENT}/${v}"
                 index=$(($index+1))
+		continue
 	    fi
             if [ ${index} -eq 1 ];then
-	        echo Play PIC ${ZINDEX_2} ${RECT_2} "${CONTENT_2}/${v}"
+	        echo Play PIC ${ZINDEX_2} ${RECT_2} "${CONTENT}/${v}"
 	        /usr/bin/xplayctl -play -libName pic -zIndex ${ZINDEX_2} -rect ${RECT_2} -path "${CONTENT}/${v}"
                 index=$(($index+1))
+		continue
 	    fi
             if [ ${index} -eq 2 ];then
-	        echo Play PIC ${ZINDEX_3} ${RECT_3} "${CONTENT_3}/${v}"
+	        echo Play PIC ${ZINDEX_3} ${RECT_3} "${CONTENT}/${v}"
 	        /usr/bin/xplayctl -play -libName pic -zIndex ${ZINDEX_3} -rect ${RECT_3} -path "${CONTENT}/${v}"
                 index=$(($index+1))
+		continue
 	    fi
             if [ ${index} -eq 3 ];then
-	        echo Play PIC ${ZINDEX_4} ${RECT_4} "${CONTENT_4}/${v}"
+	        echo Play PIC ${ZINDEX_4} ${RECT_4} "${CONTENT}/${v}"
 	        /usr/bin/xplayctl -play -libName pic -zIndex ${ZINDEX_4} -rect ${RECT_4} -path "${CONTENT}/${v}"
-                index=0 # 计数器归零
-   	        sleep 5 # SLEEP 5S
+                index=0
+   	        sleep 5
+		continue
 	    fi
 	fi 
     done
