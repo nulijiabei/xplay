d5d4864d0bd4bf75b4f186fb489d459834040e5d
nljb <nljb@qq.com>
2019-12-23 13:50:19 +0000

    优化整理 example/xplayctl.go

--- example/xplayctl.go
+++ example/xplayctl.go
@@ -1,6 +1,6 @@
 ﻿package main
 
-// 2019/11/18 v1.3
+// v1.0.8.v20191222
 
 import (
 	"encoding/json"
@@ -16,11 +16,6 @@ import (
 // Connect Address
 var addr = flag.String("addr", "127.0.0.1:8700", "xplay tcp address")
 
-// Default
-var id = flag.String("id", "", "debug id")
-var start = flag.Int64("start", -1, "start time millisecond")
-var libName = flag.String("libName", "", "video、pic、camera、gif、qrcode、text、scroll、background")
-
 // Stop
 var stop = flag.Bool("stop", false, "stop or play")
 var all = flag.Bool("all", false, "stop all")
@@ -28,21 +23,19 @@ var ids = flag.String("ids", "", "stop ids")
 
 // Play
 var play = flag.Bool("play", false, "play or stop")
+var id = flag.String("id", "", "debug id")
+var start = flag.Int64("start", -1, "start time millisecond")
+var libName = flag.String("libName", "", "video、pic、camera、gif、qrcode、text、scroll、background")
 
 // Play Params
-var zIndex = flag.Int("zIndex", 10, "1-9999")
+var zIndex = flag.Int("zIndex", 10, "1-999")
 var rect = flag.String("rect", "0,0,1920,1080", "")
 var screen_mode = flag.String("screen_mode", "landscape", "landscape、portrait")
 var screen_rotate = flag.Int("screen_rotate", 0, "landscape: 0、180 or portrait: 90、270")
 var path = flag.String("path", "", "file path")
 var content = flag.String("content", "", "data content")
 
-// Play Camera
-var device = flag.String("device", "/dev/video0", "camera device")
-var camera_width = flag.Int("camera_width", 1280, "camera video width")
-var camera_height = flag.Int("camera_height", 720, "camera video height")
-
-// Play Text
+// Text
 var font_ttf = flag.String("font_ttf", "", "TrueTypeFont")
 var font_size = flag.Int("font_size", 18, "")
 var color = flag.String("color", "rgba(0,128,0,100%)", "")
@@ -50,11 +43,16 @@ var bgcolor = flag.String("bgcolor", "rgba(0,0,0,20%)", "")
 var align = flag.String("align", "center", "center、right、left")
 var style = flag.String("style", "normal", "normal、bold、italic、underline、strikethrough")
 
-// Play Scroll
+// Camera
+var device = flag.String("device", "/dev/video0", "camera device")
+var camera_width = flag.Int("camera_width", 1280, "camera video width")
+var camera_height = flag.Int("camera_height", 720, "camera video height")
+
+// Scroll
 var speed = flag.Int("speed", 1, "move pixel / frame")
 var orientation = flag.String("orientation", "horizontal", "horizontal | vertical")
 
-// Play Toast
+// Toast
 var toast_type = flag.String("toast_type", "notice", "notice、success、warning、error")
 var duration = flag.Int("duration", 0, "timeout stop toast")
 
@@ -109,35 +107,68 @@ func (this *XPlay) stop(_ids string) error {
 	return this.send(data)
 }
 
-func (this *XPlay) params(_zIndex int, _x, _y, _width, _height int, _screen_mode string, _screen_rotate int) map[string]interface{} {
-	params := make(map[string]interface{})
-	params["zIndex"] = _zIndex
-	params["top"] = _y
-	params["left"] = _x
-	params["width"] = _width
-	params["height"] = _height
-	params["screen_mode"] = _screen_mode
-	params["screen_rotate"] = _screen_rotate
-	return params
-}
-
 func (this *XPlay) play() error {
-	var params map[string]interface{}
+	params := make(map[string]interface{})
 	rs := strings.Split(*rect, ",")
 	x, _ := strconv.Atoi(rs[0])
 	y, _ := strconv.Atoi(rs[1])
 	width, _ := strconv.Atoi(rs[2])
 	height, _ := strconv.Atoi(rs[3])
-	params = this.params(*zIndex, x, y, width, height, *screen_mode, *screen_rotate)
-	if (*libName) == "video" || (*libName) == "pic" || (*libName) == "gif" {
+	if (*libName) == "video" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["path"] = *path
+	} else if (*libName) == "pic" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
+		params["path"] = *path
+	} else if (*libName) == "gif" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
+		params["path"] = *path
+	} else if (*libName) == "qrcode" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
+		params["content"] = *content
 	} else if (*libName) == "camera" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["device"] = *device
 		params["camera_width"] = *camera_width
 		params["camera_height"] = *camera_height
-	} else if (*libName) == "qrcode" {
-		params["content"] = *content
 	} else if (*libName) == "text" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["content"] = *content
 		params["font_ttf"] = *font_ttf
 		params["font_size"] = *font_size
@@ -146,6 +177,13 @@ func (this *XPlay) play() error {
 		params["align"] = *align
 		params["style"] = *style
 	} else if (*libName) == "scroll" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["content"] = *content
 		params["font_ttf"] = *font_ttf
 		params["font_size"] = *font_size
@@ -154,6 +192,13 @@ func (this *XPlay) play() error {
 		params["orientation"] = *orientation
 		params["speed"] = *speed
 	} else if (*libName) == "datetime" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["font_ttf"] = *font_ttf
 		params["font_size"] = *font_size
 		params["color"] = *color
@@ -161,10 +206,20 @@ func (this *XPlay) play() error {
 		params["align"] = *align
 		params["style"] = *style
 	} else if (*libName) == "toast" {
+		params["zIndex"] = *zIndex
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["content"] = *content
 		params["toast_type"] = *toast_type
 		params["duration"] = *duration
 	} else if (*libName) == "background" {
+		params["zIndex"] = *zIndex
+		params["top"] = x
+		params["left"] = y
+		params["width"] = width
+		params["height"] = height
+		params["screen_mode"] = *screen_mode
+		params["screen_rotate"] = *screen_rotate
 		params["bgcolor"] = *bgcolor
 	}
 	data := make(map[string]interface{})
