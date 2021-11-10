a3a03db9f895a72aaea5699f5ddd4913148ffc91
努力加贝 <nulijiabei@users.noreply.github.com>
2021-11-10 16:47:11 +0800

    Update rpi_omx_install.sh

--- raspios-lite/rpi_omx_install.sh
+++ raspios-lite/rpi_omx_install.sh
@@ -2,7 +2,7 @@
 
 dir=$(/usr/bin/dirname $0)
 
-sh -x ${dir}/../buster/rpi_omx_install.sh
+sh -x ${dir}/../raspios/rpi_omx_install.sh
 
 cp -aRrf ${dir}/libs/rpi_omx/* /usr/local/lib/
 for lib in `ls ${dir}/libs/rpi_omx/`;do
